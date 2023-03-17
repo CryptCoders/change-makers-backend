525cc698da0b5841cbf47afd54c5d4efda62451b
JoKeR-VIKING <prathamvasani1@gmail.com>
2023-03-17 11:15:27 +0530

    removed middleware, regex and site bugs

--- src/routes.ts
+++ src/routes.ts
@@ -9,10 +9,10 @@ const BASE_PATH = '/api/v1';
 export default (app: Application) => {
     const routes = () => {
         app.use(BASE_PATH, authRoutes.routes());
-        app.use(BASE_PATH, authMiddleware.verifyUser, authRoutes.signoutRoute());
-
-        app.use(BASE_PATH, authMiddleware.verifyUser, siteRoutes.site());
         app.use(BASE_PATH, siteRoutes.get());
+
+        app.use(BASE_PATH, authMiddleware.verifyUser, authRoutes.signoutRoute());
+        app.use(BASE_PATH, authMiddleware.verifyUser, siteRoutes.site());
         app.use(BASE_PATH, eventRoutes.routes());
     };
 
